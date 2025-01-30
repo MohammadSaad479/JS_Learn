543bd3bf885608f847fb0ce3a1fae5da69eb8051
MohammadSaad479 <saadqureshi479@gmail.com>
2025-01-30 10:06:18 +0000

    Basics

---
+++ Basics.js
@@ -0,0 +1,10 @@
+console.log("Hey Saad");
+ 
+const CustomerID = "7862"
+let customerCity = "Delhi"
+let customerAge = "25"
+let customerChoice
+var customerOrder = "5"
+customerEmail = "saad@123.com"
+
+console.table([CustomerID, customerAge,customerCity,customerOrder,customerEmail,customerChoice])
