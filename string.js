a87295ffc2b13dbd3cf1ec5d7c42bc0b9a3922fa
MohammadSaad479 <saadqureshi479@gmail.com>
2025-02-03 10:36:33 +0000

    string prototype

---
+++ string.js
@@ -0,0 +1,20 @@
+ /*const myFname = "Mohammad"
+const myLname = "Saad"
+
+ const School = "JAMIA MILLIA ISLAMIA"
+
+ console.log(`my name is ${myFname} ${myLname} and I am completed my schooling from ${School}`); */
+
+ const myCountry = "i love my india"
+
+ console.log(myCountry.toUpperCase());
+ console.log(myCountry.length);
+ console.log(myCountry.charAt(2));
+ console.log(myCountry.includes("love"));
+ console.log(myCountry.replace("love", "hate"));
+ console.log(myCountry.substring(3));
+ 
+ 
+
+ 
+ 
