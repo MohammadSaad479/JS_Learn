f5e81596a6707d2826a6ff68c39c1b7d02bf6f2e
MohammadSaad479 <saadqureshi479@gmail.com>
2025-02-04 10:08:48 +0000

    date and time

---
+++ date_time.js
@@ -0,0 +1,19 @@
+ let mydate = new Date()
+//  console.log(mydate.toString());
+//  console.log(mydate.toLocaleString());
+//  console.log(mydate.toLocaleDateString());
+//  console.log(mydate.toTimeString());
+//  console.log(mydate.getTimezoneOffset());
+ 
+// let myNewDate = Date(2025,1,4)
+// console.log(myNewDate);
+
+// let  myNewDate = new Date("2025-02-6")
+// console.log(myNewDate.toLocaleString());
+
+
+
+ 
+
+ 
+ 
