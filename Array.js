5e4731464938a10897a061d85b8fa9e68fa84f0d
MohammadSaad479 <saadqureshi479@gmail.com>
2025-02-04 10:57:19 +0000

    Arrays

---
+++ Array.js
@@ -0,0 +1,72 @@
+// const myArr = [1,2,3,4,5]
+// const myMarvel = ["ironman", "captain america", "Thanos", "Thor"]
+// const myArr1 = Array (1,2,3,4,5,6)
+
+// console.log(myArr1[3]);
+
+// const myArr = [1, 2 , 3 , 4 , 5 , 6 , 7]
+
+// Array Methods
+
+// myArr.push(8)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+//  console.log(myArr.includes(11));
+ 
+//  console.log(myArr.indexOf(5));
+
+//  console.log(typeof myArr);
+
+//  const newArr = myArr.join()  // converting array into string
+//  console.log(newArr);
+//  console.log(typeof newArr);
+
+// Slice and Splice
+
+// console.log("A", myArr);
+
+
+// const newArr2 = myArr.slice(1,3)
+// console.log(newArr2);
+
+// const marvelHeros = ["ironman", "thor", "spiderman"]
+// const dcHeros = ["batman", "superman", "wonderwoman"]
+
+// const allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+//  const allHeros = [...marvelHeros , ...dcHeros] // easy way for concat
+//  console.log(allHeros);
+
+// const nayaArr = [1,2,3,[4,5],6,7 ,[9,10,[11,12,]],13,14]
+
+// const nayaArr2 = nayaArr.flat(3) // 3 number show the depth of the array
+// console.log(nayaArr2);
+
+
+console.log(Array.from('mohammad'));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+ 
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+
