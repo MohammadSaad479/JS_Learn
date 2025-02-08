4d56fb344965f96c6fd68bcb00914ab6f76ddc94
MohammadSaad479 <saadqureshi479@gmail.com>
2025-02-08 10:49:57 +0000

    functions

---
+++ functions.js
@@ -0,0 +1,79 @@
+// function sayHello() {
+//     console.log("ALLAH IS WATCHING YOU");    
+// }
+// sayHello()
+
+// function multitwoNum(num1, num2){
+
+//     return num1*num2
+// }
+
+// console.log(multitwoNum(3,4));
+
+// function AddtwoNum(a,b){
+
+//     let result = a+b
+//     console.log(result);
+// }
+
+// AddtwoNum(3,4)
+
+
+// function AddtwoNum(a,b){
+
+//     console.log(a+b);    
+// }
+// AddtwoNum(3,4)
+
+
+// function checkEven(number) {
+
+//     if( number%2==0){
+//         console.log(`number is even : ${number}`);
+//     }
+//     else{
+//         console.log(`number is odd : ${number}`);
+//     }
+//     return number   
+// }
+// console.log(checkEven(4));
+
+// *********** Functions with objects and arrays***************
+
+// const user = {
+//     name : "Saad",
+//     age : 24,
+//     city : "New Delhi"
+// }
+
+// function checkObject (anyobject){
+//     console.log(`user name is ${anyobject.name} and the age is ${anyobject.age} and the city is ${anyobject.city}`);
+    
+// }
+
+// checkObject(user)
+
+// checkObject({      // we can use object like this 
+//     name : "prince",
+//     age : 18,
+//     city : "mumbai"
+// })
+
+
+// const newArray = [100,200,300,400]
+
+// function checkArray(getArray) 
+// {
+//    return getArray
+      
+// }
+
+// // console.log(checkArray(newArray));
+
+// console.log(checkArray(newArray[2]));
+
+
+
+
+
+
